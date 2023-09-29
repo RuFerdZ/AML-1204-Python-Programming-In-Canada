0314624a443ec72d71f763ac141152385ec6d3a3
Rusiru Fernando <ge.rusiru@gmail.com>
2023-09-29 08:36:06 -0400

    week-02: variables and datatypes notes

---
+++ week_02_variables_and_datatypes/variables_and_datatypes.py
@@ -0,0 +1,26 @@
+# Introduction to variables and datatypes
+
+# Variables
+# Variables are used to store values
+# Variables are created when a value is assigned to it
+# Variables do not need to be declared with any particular type and can even change type after they have been set
+# Variable names are case sensitive
+# Variable names can be any length
+# Variable names can contain letters, numbers, and the underscore character
+# Variable names cannot start with a number
+# Variable names cannot contain spaces
+# Variable names cannot contain special characters
+# Variable names should be descriptive, without being too long
+
+# Variable assignment
+x = 5
+
+# Daa types
+# Python has the following data types built-in by default, in these categories:
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
