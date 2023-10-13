1ccf358051359fc2d3a8fa6daee1154a3142f402
RuFerdZ <ge.rusiru@gmail.com>
2023-10-13 09:33:50 -0400

    week-06: added basic arithmetic operations file

---
+++ week_06_arithmetic_operations/01_basic_operators.py
@@ -0,0 +1,62 @@
+# Basic arithmetic operations
+
+# Addition
+print(2 + 2)
+
+# Subtraction
+print(2 - 2)
+
+# Multiplication
+print(2 * 2)
+
+# Division
+print(2 / 2)
+
+# Floor Division
+print(2 // 2)
+
+# Modulus
+print(2 % 2)
+
+# Exponentiation
+print(2 ** 2)
+
+# Assignment operators
+# =
+# +=
+# -=
+# *=
+# /=
+# %=
+# **=
+# //=
+
+# Comparison operators
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+# Logical operators
+# and
+# or
+# not
+
+# Identity operators
+# is
+# is not
+
+# Membership operators
+# in
+# not in
+
+# Bitwise operators
+# &
+# |
+# ^
+# ~
+# <<
+# >>
+
